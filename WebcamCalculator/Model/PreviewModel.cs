cfcecf802be359f15f9f9fec3a37837eed82883c
Mateusz Kapiszewski <kapiszewski.mateusz@gmail.com>
2017-11-03 22:31:12 +0100

    Camera previews, winforms host

--- WebcamCalculator/Model/PreviewModel.cs
+++ WebcamCalculator/Model/PreviewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Expression.Encoder.Devices;
 
 namespace WebcamCalculator.Model
 {
     class PreviewModel
     {
+
+        public Collection<EncoderDevice> VideoDevices { get; set; }
+        public Collection<EncoderDevice> AudioDevices { get; set; }
         public PreviewModel()
         {
             CameraCapture();
